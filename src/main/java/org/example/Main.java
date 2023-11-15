e274e3cc7de372e23c6a6c86481f36a67f2f9ce9
02prem <prem30102002@gmail.com>
2023-11-15 19:20:27 +0530

    checking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        
             System.out.println("Calculator Menu:");
             System.out.println("1. Addition");
             System.out.println("2. Subtraction");
